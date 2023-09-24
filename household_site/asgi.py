ce8eb8fac316cde73893ac9c710e17a06f9b8628
aulia.hariz <aulia.hariz@psn.co.id>
2023-09-24 09:00:40 +0700

    add asgi

--- household_site/asgi.py
+++ household_site/asgi.py
@@ -7,8 +7,10 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 """
 
-import os
+import sys
+sys.path.insert(0, "/home/harizcor/app/household/household_site")
 
+import os
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'household_site.settings')
