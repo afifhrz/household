01df91e4b9eaf82849361bc54239e7449432c5d8
aulia.hariz <aulia.hariz@psn.co.id>
2023-09-25 20:51:52 +0700

    add new files

--- household_site/settings.py
+++ household_site/settings.py
@@ -29,7 +29,11 @@ SECRET_KEY = os.environ['SECRET_KEY']
 DEBUG = True
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 STATIC_ROOT= os.environ['STATIC_ROOT']
+=======
+STATIC_ROOT= '/home/harizcor/household/household_site/public/'
+>>>>>>> cefdc451af01c02e8f01144a5e80d0ab3e267e78
 =======
 STATIC_ROOT= '/home/harizcor/household/household_site/public/'
 >>>>>>> cefdc451af01c02e8f01144a5e80d0ab3e267e78
